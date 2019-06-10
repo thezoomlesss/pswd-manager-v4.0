f55880ec3cfbde7750959755ba40229625c74fc7
Mohamad Zabad <zabadmohamad@yahoo.com>
2019-06-10 16:55:10 +0200

    Simple login page added

--- pswd-manager/screens/HomeScreen2.js
+++ pswd-manager/screens/HomeScreen2.js
@@ -7,7 +7,8 @@ import {
   Text,
   TouchableOpacity,
   View,
-  Button
+  Button,
+  TextInput
 } from 'react-native';
 import { WebBrowser } from 'expo';
 
@@ -17,9 +18,16 @@ export default class HomeScreen2 extends React.Component {
   static navigationOptions = {
     header: null,
   };
-
+  constructor(props) {
+    super(props);
+    
+    this.auth_login = this.auth_login.bind(this);
+  }
+  auth_login() {
+    console.log('Hello!');
+  }
   render() {
-    const {navigate} = this.props.navigation;
+    const { navigate } = this.props.navigation;
     return (
       <View style={styles.container}>
         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
@@ -37,24 +45,19 @@ export default class HomeScreen2 extends React.Component {
             title="Go to Home1"
             onPress={() => navigate('Home', { name: 'Home1' })}
           />
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
 
-            <Text style={styles.getStartedText}>Get startedstartedstartedstarted by opening</Text>
-
-            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
-            </View>
-
-            <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
+          <View style={styles.loginContainer}>
+            <Text
+              style={{ fontSize: 27 }}>
+              Login
             </Text>
-          </View>
-
-          <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-            </TouchableOpacity>
+            <TextInput style={styles.input} placeholder='Username' />
+            <TextInput style={styles.input} placeholder='Password' />
+            <View style={{ margin: 7 }} />
+            <Button
+              onPress={this.auth_login}
+              title="Submit"
+            />
           </View>
         </ScrollView>
 
@@ -69,38 +72,9 @@ export default class HomeScreen2 extends React.Component {
     );
   }
 
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
 
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
 
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
 
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
-  };
 }
 
 const styles = StyleSheet.create({
@@ -108,13 +82,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
+
   contentContainer: {
     paddingTop: 30,
   },
@@ -130,13 +98,6 @@ const styles = StyleSheet.create({
     marginTop: 3,
     marginLeft: -10,
   },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
   codeHighlightText: {
     color: 'rgba(96,100,109, 0.8)',
   },
@@ -144,12 +105,6 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(0,0,0,0.05)',
     borderRadius: 3,
     paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
   },
   tabBarInfoContainer: {
     position: 'absolute',
@@ -176,18 +131,16 @@ const styles = StyleSheet.create({
     color: 'rgba(96,100,109, 1)',
     textAlign: 'center',
   },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
+  loginContainer: {
+    flex: 1,
+    borderColor: '#eb144c',
+    borderWidth: 1,
     alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20
   },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+  input: {
+    width: '100%',
+    textAlign: 'center'
+  }
 });
